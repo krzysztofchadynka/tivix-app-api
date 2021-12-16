193504a8c6ae94da2ade046c1454573eda568a90
Krzysztof Chadynka <kchadynka@o2.pl>
2021-12-17 00:28:41 +0100

    Preapare seeder; create build script (#6)
    Co-authored-by: Krzysztof Chadynka <krzysztof@macbook-air-krzysztof.home>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Repositories\UserRepository;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(UserRepository $userRepository)
     {
-        // \App\Models\User::factory(10)->create();
+        $userRepository->create([
+            'name' => 'test-user',
+            'email' => 'test-user@tivix.com',
+            'password' => Hash::make('test1234'),
+        ]);
     }
 }
